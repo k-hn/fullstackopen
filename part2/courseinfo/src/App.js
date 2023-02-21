6b5a3c51cf5e531f9ed3621eb2ffbedb03f61ae4
Hilary <125813796+k-hn@users.noreply.github.com>
2023-02-21 19:08:38 +0000

    add exercise 2.1 solution

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,76 @@
+const Header = ({ course }) => <h1>{course.name}</h1>
+
+const Total = (props) => {
+  const exerciseArr = props.course.parts.map(part => part.exercises)
+  const sum = exerciseArr.reduce(
+    (acc, currentValue) => acc + currentValue, 0
+  )
+
+  return (
+    <p>Number of exercises {sum}</p>
+  )
+}
+
+const Part = ({ part }) =>
+  <p>
+    {part.name} {part.exercises}
+  </p>
+
+const Content = (props) => {
+  const parts = props.course.parts
+  return (
+    <>
+      <Part
+        part={parts[0]}
+      />
+      <Part
+        part={parts[1]}
+      />
+      <Part
+        part={parts[2]}
+      />
+    </>
+  )
+}
+
+
+const Course = (props) => {
+  const { course } = props
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  )
+
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
