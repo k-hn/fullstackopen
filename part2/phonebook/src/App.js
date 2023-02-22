58019c4605ab39e44fe29b19294fe2aab1418b7b
Hilary <125813796+k-hn@users.noreply.github.com>
2023-02-22 12:29:44 +0000

    add exercise 2.9 solution

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: "39-44-5323523" }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState("")
+  const [searchContact, setSearchContact] = useState("")
 
   const handleNewName = (event) => {
     setNewName(event.target.value)
@@ -13,27 +17,46 @@ const App = () => {
 
   const addName = (event) => {
     event.preventDefault()
-    const currentNameObject = {
-      name: newName,
-      number: newNumber
+
+    // Validate input
+    if (!validInputLengths(newName, newNumber)) {
+      alert(`Please enter values for both name and number inputs`)
+      return
     }
 
     if (nameExists(persons, newName)) {
       alert(`${newName} is already added to the phonebook`)
-    } else {
-      setPersons(persons.concat(currentNameObject))
-      setNewName("")
-      setNewNumber("")
+      return
     }
+
+    const currentNameObject = {
+      name: newName,
+      number: newNumber
+    }
+    setPersons(persons.concat(currentNameObject))
+    setNewName("")
+    setNewNumber("")
+
+  }
+
+  const validInputLengths = (contactName, contactNumber) => {
+    return contactName.length > 0 && contactNumber.length > 0
   }
 
   const getFormattedContacts = (contacts) => {
-    return contacts.map(contact => {
+    const filteredContacts = getFilteredContacts(contacts, searchContact)
+    return filteredContacts.map(contact => {
       return <tr key={contact.name}>
         <td>{contact.name}</td>
         <td>{contact.number}</td>
       </tr>
     })
+  }
+
+  const getFilteredContacts = (contacts, searchTerm) => {
+    return contacts.filter(
+      contact => contact.name.toLowerCase().includes(searchTerm.toLowerCase())
+    )
   }
 
   const nameExists = (contactsList, name) => {
@@ -45,9 +68,22 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearch = (event) => {
+    const searchTerm = event.target.value
+    setSearchContact(searchTerm)
+  }
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        filter show with <input onChange={handleSearch} />
+      </div>
+
+
+
+
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNewName} />
@@ -62,6 +98,8 @@ const App = () => {
         </div>
 
       </form>
+
+
       <h2>Numbers</h2>
       <table>
         <tbody>
