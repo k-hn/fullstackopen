4c4e34b94a5846ab07b5253b57ac9d80eabfd88f
Hilary <125813796+k-hn@users.noreply.github.com>
2023-02-23 15:05:34 +0000

    add exercise 2.15 solution

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -29,6 +29,12 @@ const App = () => {
   const addName = (event) => {
     event.preventDefault()
 
+    const currentNameObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1
+    }
+
     // Validate input
     if (!validInputLengths(newName, newNumber)) {
       alert(`Please enter values for both name and number inputs`)
@@ -36,35 +42,62 @@ const App = () => {
     }
 
     if (nameExists(persons, newName)) {
-      alert(`${newName} is already added to the phonebook`)
+      if (window.confirm(`${newName} is already added to phoneboook, replace the old number with a new one?`)) {
+        updateName(currentNameObject)
+      }
       return
-    }
-
-    const currentNameObject = {
-      name: newName,
-      number: newNumber,
-      id: persons.length + 1
     }
 
     personsService
       .create(currentNameObject)
       .then(createdContact => {
-        setPersons(persons.concat(currentNameObject))
-        setNewName("")
-        setNewNumber("")
+        setPersons(persons.concat(createdContact))
+        clearNewContactInputs()
       })
       .catch(error => {
         alert(`There was an error: ${error}`)
       })
   }
 
+  const updateName = (newPersonObj) => {
+    // Update an existing contact's information
+    const existingContact = getContact(persons, newPersonObj.name)
+    const updatedContact = {
+      ...existingContact,
+      number: newPersonObj.number
+    }
+
+    personsService
+      .updatePerson(updatedContact)
+      .then((receivedPerson) => {
+        setPersons(persons.map(p => p.id === receivedPerson.id ? receivedPerson : p))
+        clearNewContactInputs()
+      })
+      .catch(error => {
+        alert(`An error occurred: ${error}`)
+      })
+  }
+
+  const clearNewContactInputs = () => {
+    // Clears the `name` and `number` inputs for adding a new person
+    setNewName("")
+    setNewNumber("")
+  }
+
   const validInputLengths = (contactName, contactNumber) => {
+    // Checks if name and number are provided when adding a new contact
     return contactName.length > 0 && contactNumber.length > 0
   }
 
   const nameExists = (contactsList, name) => {
+    // Checks if the provided name exists in the provided contact list
     const contacts = contactsList.map(contactObj => contactObj.name)
     return contacts.includes(name)
+  }
+
+  const getContact = (contacts, name) => {
+    // Returns the contact object that matches the provided name 
+    return contacts.find((contact) => contact.name === name)
   }
 
   const handleNewNumber = (event) => {
--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -2,19 +2,23 @@ import axios from "axios";
 const baseURL = "http://localhost:3001/persons"
 
 const getAll = () => {
-    const request = axios.get(baseURL)
-    return request.then(response => response.data)
+  const request = axios.get(baseURL)
+  return request.then(response => response.data)
 }
 
 const create = (personObj) => {
-    const request = axios.post(baseURL, personObj)
-    return request.then(response => response.data)
+  const request = axios.post(baseURL, personObj)
+  return request.then(response => response.data)
 }
 
 const deletePerson = (id) => {
-    return axios.delete(`${baseURL}/${id}`)
-
+  return axios.delete(`${baseURL}/${id}`)
 }
 
-const personsService = { getAll, create, deletePerson }
+const updatePerson = (updatedPersonObj) => {
+  const request = axios.put(`${baseURL}/${updatedPersonObj.id}`, updatedPersonObj)
+  return request.then(response => response.data)
+}
+
+const personsService = { getAll, create, deletePerson, updatePerson }
 export default personsService
