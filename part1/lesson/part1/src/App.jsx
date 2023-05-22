a855f59d867ee8f1ded8b1ee1ac1b4a0baf31e25
Hilary <125813796+k-hn@users.noreply.github.com>
2023-05-22 14:55:28 +0000

    add 1d

--- part1/lesson/part1/src/App.jsx
+++ part1/lesson/part1/src/App.jsx
@@ -1,38 +1,57 @@
 import { useState } from "react";
 
-const Display = ({ counter }) => {
-  return (
-    <div>{counter}</div>
-  )
-}
-
-const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
-
-const App = (props) => {
-  const [counter, setCounter] = useState(0);
-  console.log("rendering with couter values");;
-
-  const increaseByOne = () => {
-    console.log("increasing, value before", counter);
-    setCounter(counter + 1);
-  }
-
-  const decreaseByOne = () => {
-    console.log("decreasing, value before", counter);
-    setCounter(counter - 1);
-  }
-
-  const setToZero = () => {
-    console.log("resetting to zero, value before", counter);
-    setCounter(0);
+const History = (props) => {
+  if (props.allClicks.length == 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
   }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button handleClick={increaseByOne} text="plus" />
-      <Button handleClick={setToZero} text="zero" />
-      <Button handleClick={decreaseByOne} text="minus" />
+      button press history: {props.allClicks.join("  ")}
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAllClicks] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const handleLeftClicks = () => {
+    setAllClicks(allClicks.concat("L"));
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    setTotal(updatedLeft + right);
+  }
+
+  const handleRightClicks = () => {
+    setAllClicks(allClicks.concat("R"));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
+  }
+
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClicks} text="left" />
+      <Button handleClick={handleRightClicks} text="right" />
+      {right}
+      <p>total: {total}</p>
+      <History allClicks={allClicks} />
     </div>
   )
 }
