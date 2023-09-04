ed44729b0a70fc66155f564c84cd155e5f323fbc
Hilary <125813796+k-hn@users.noreply.github.com>
2023-09-04 21:06:49 +0000

    [lesson] 1d: A more complex state, debugging React apps

--- part1/lesson/part1/src/App.jsx
+++ part1/lesson/part1/src/App.jsx
@@ -1,45 +1,57 @@
 import { useState } from "react"
 
-const Display = ({counter}) => <div>{counter}</div>
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+	return (
+	    <div>
+		the app is used by pressing the buttons
+	    </div>
+	)
+    }
 
-const Button = ({text, handleClick}) => <button onClick={handleClick}>{text}</button>
+    return (
+	<div>
+	    button press history: {props.allClicks.join(" ")}
+	</div>
+    )
+}
+
+const Button = ({handleClick, text}) => {
+    return (
+	<button onClick={handleClick}>
+	    {text}
+	</button>
+    )
+}
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  console.log("rendering with counter value", counter);
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+    const [allClicks, setAllClicks] = useState([])
+    const [total, setTotal] = useState(0)
 
-    const increaseByOne = () => {
-	console.log("increasing, value before", counter);
-	setCounter(counter + 1)
-    }
-    
-    const setToZero = () => {
-	console.log("resetting to zero, value before", counter)
-	setCounter(0)
-    }
-    
-    const decreaseByOne = () => {
-	console.log("decreasing, value before", counter)
-	setCounter(counter - 1)
+    const handleLeftClick = () => {
+	setAllClicks(allClicks.concat("L"))
+	const updatedLeft = left + 1
+	setLeft(updatedLeft)
+	setTotal(updatedLeft + right)
     }
 
-  return (
-    <>
-      <Display counter={counter} />
-      <Button
-        handleClick={increaseByOne}
-        text="plus"
-      />
-      <Button
-        handleClick={setToZero}
-        text="zero"
-      />
-      <Button
-        handleClick={decreaseByOne}
-        text="minus"
-      />
-    </>
-  )
+    const handleRightClick = () => {
+	setAllClicks(allClicks.concat("R"))
+	const updatedRight = right + 1
+	setRight(updatedRight)
+	setTotal(left + updatedRight)
+    }
+    
+    return (
+	<div>
+	    <Button handleClick={handleLeftClick} text="left" />
+	    <Button handleClick={handleRightClick} text="right" />
+	    <History allClicks={allClicks} />
+	    <p>total {total}</p>
+	</div>
+    )
 }
 
 export default App
