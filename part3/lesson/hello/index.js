7930d523d1e0c32ac250ca43da8b73bdde9d76df
k-hn <125813796+k-hn@users.noreply.github.com>
2023-10-20 15:09:58 +0000

    [lesson] middleware

--- part3/lesson/hello/index.js
+++ part3/lesson/hello/index.js
@@ -1,7 +1,22 @@
 const express = require("express")
 const app = express()
 
+
+const requestLogger = (request, response, next) => {
+  console.log("Method: ", request.method)
+  console.log("Path: ", request.path)
+  console.log("Body: ", request.body)
+  console.log("---")
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error: "unknown endpoint"})
+}
+
 app.use(express.json())
+app.use(requestLogger)
+app.use(unknownEndpoint)
 
 let notes = [
   {
@@ -76,6 +91,8 @@ app.post("/api/notes", (request, response) => {
   response.json(note)
 })
 
+
+// Middleware
 
 const PORT = 3001
 app.listen(PORT)
