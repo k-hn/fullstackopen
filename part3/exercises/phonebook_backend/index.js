52bbb6f59b7ac1e1d587d3d63be47c09c19d6b9c
k-hn <125813796+k-hn@users.noreply.github.com>
2023-10-23 23:28:20 +0000

    [exercise] 3.15 Phonebook database, step3

--- part3/exercises/phonebook_backend/index.js
+++ part3/exercises/phonebook_backend/index.js
@@ -65,10 +65,20 @@ app.get("/api/persons/:id", (request, response) => {
 
 
 app.delete("/api/persons/:id", (request, response) => {
+  /*
   const id = Number(request.params.id)
   persons = persons.filter(person => person.id !== id)
 
   return response.status(204).end()
+*/
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: "malformed id" })
+    })
 })
 
 
