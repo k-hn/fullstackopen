934689ac66f6d2d6a131384cbe3be08b202cdec5
k-hn <125813796+k-hn@users.noreply.github.com>
2023-10-25 13:05:45 +0000

    [exercise] 4.4: helper functions and unit tests, step2

--- part4/exercises/bloglist/tests/list_helper.test.js
+++ part4/exercises/bloglist/tests/list_helper.test.js
@@ -6,3 +6,59 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', () => {
+  const listWithNoBlogs = []
+  const listWithOneBlog = [
+    {
+      'title': 'The Case of a Curious SQL Query',
+      'author': 'Justin Jaffray',
+      'url': 'https://buttondown.email/jaffray/archive/the-case-of-a-curious-sql-query/',
+      'likes': 21,
+      'id': '6538def56643a9cd5791bbdf'
+    }
+  ]
+  const listWithMultipleBlogs = [
+    {
+      'title': 'Embeddings: What they are and why they matter',
+      'author': 'Simon Willison',
+      'url': 'https://simonwillison.net/2023/Oct/23/embeddings/',
+      'likes': 228,
+      'id': '6538dd33e01ab90945153431'
+    },
+    {
+      'title': 'On Learning Compilers and Creating Programming Languages',
+      'author': 'Craig Stuntz',
+      'url': 'https://www.craigstuntz.com/posts/2023-10-13-learning-compilers-and-programming-languages.html',
+      'likes': 41,
+      'id': '6538deb76643a9cd5791bbdd'
+    },
+    {
+      'title': 'The Case of a Curious SQL Query',
+      'author': 'Justin Jaffray',
+      'url': 'https://buttondown.email/jaffray/archive/the-case-of-a-curious-sql-query/',
+      'likes': 21,
+      'id': '6538def56643a9cd5791bbdf'
+    }
+  ]
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes(listWithNoBlogs)
+
+    expect(result).toBe(0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+
+    expect(result).toBe(21)
+  })
+
+  test('of a bigger list is calculated right', () => {
+
+
+    const result = listHelper.totalLikes(listWithMultipleBlogs)
+
+    const sumOfLikes = 290
+    expect(result).toBe(sumOfLikes)
+  })
+})
--- part4/exercises/bloglist/utils/list_helper.js
+++ part4/exercises/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const sumLikes = blogs.reduce((acc, blog) => {
+    return acc + blog.likes
+  }, 0)
+
+  return sumLikes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
