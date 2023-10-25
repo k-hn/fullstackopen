b84b1c966894002f958098e69956f5341892cc3f
k-hn <125813796+k-hn@users.noreply.github.com>
2023-10-25 09:50:29 +0000

    [exercise] 4.2: Blog list, step2

---
+++ part4/exercises/bloglist/app.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to ', process.env.MONGODB_URI)
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB: ', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
