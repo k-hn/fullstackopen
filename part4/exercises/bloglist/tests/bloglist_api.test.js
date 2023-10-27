0ff8390629d58050b998d9c3d7eebde58091833a
k-hn <125813796+k-hn@users.noreply.github.com>
2023-10-27 13:03:25 +0000

    [exercise] 4.8: Blog list tests, step1

---
+++ part4/exercises/bloglist/tests/bloglist_api.test.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany()
+  await Blog.insertMany(helper.initialBloglist)
+})
+
+afterAll(async () => {
+  await mongoose.connection.close()
+})
+
+test('blog list is returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all blogs are returned', async () => {
+  const response = await api
+    .get('/api/blogs')
+
+  expect(response.body).toHaveLength(helper.initialBloglist.length)
+})
+
+
+
+
