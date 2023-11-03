9ac0bea00152cb52f75d913be5a318590256660e
k-hn <125813796+k-hn@users.noreply.github.com>
2023-11-03 17:40:50 +0000

    [exercise] 4.19: bloglist expansion, step7

--- part4/exercises/bloglist/controllers/blogs.js
+++ part4/exercises/bloglist/controllers/blogs.js
@@ -1,6 +1,16 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -25,7 +35,11 @@ blogsRouter.post('/', async (request, response) => {
   }
 
   // assign blog to user named root
-  const user = await User.findOne({ username: 'root' })
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (decodedToken.id === undefined) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
   request.body.user = user.id
 
   const blog = new Blog(request.body)
